fbb2579548faef2b1a2e26b8fadb3f52f6478780
Vladimir Irkhin <ivb1989@yandex.ru>
2022-07-01 15:22:55 +0300

    Add serializers for three table.

---
+++ download/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Client, Organization, Bill
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для сериализации данных таблицы Client
+    """
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для сериализации данных таблицы Organization
+    """
+    class Meta:
+        model = Organization
+        fields = '__all__'
+
+
+class BillSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для сериализации данных таблицы Bill
+    """
+    class Meta:
+        model = Bill
+        fields = '__all__'
